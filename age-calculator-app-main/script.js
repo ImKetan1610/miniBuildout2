e9170c68d64b0c1e3fbc02bca345d2a7f604f68c
ImKetan1610 <ketan.makode@gmail.com>
2024-05-11 11:54:38 +0530

    addede script

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -1,41 +1,131 @@
-// script.js
+const dayIn = document.getElementById("dayIn");
+const monthIn = document.getElementById("monthIn");
+const yearIn = document.getElementById("yearIn");
 
-const ageForm = document.getElementById("ageForm");
-const resultDiv = document.getElementById("result");
-const yearsDiv = document.getElementById("years");
-const monthsDiv = document.getElementById("months");
-const daysDiv = document.getElementById("days");
+const dayOut = document.getElementById("dayOut");
+const monthOut = document.getElementById("monthOut");
+const yearOut = document.getElementById("yearOut");
 
-ageForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const dob = new Date(ageForm.dob.value);
-  const today = new Date();
+const calculateBtn = document.getElementById("calculateBtn");
+const errorStyle = "0.5px solid  hsl(0, 100%, 67%)";
 
-  // Check if the date is in the future
-  if (dob > today) {
-    alert("Date of Birth cannot be in the future");
+calculateBtn.addEventListener("click", () => {
+  const D = dayIn.value;
+  const M = monthIn.value;
+  const Y = yearIn.value;
+  const dob = `${Y}-${M}-${D}`;
+
+  if (!validateDay() && !validateMonth() && !validateYear()) {
     return;
   }
 
-  // Calculate age
-  const ageInMilliseconds = today - dob;
-  const ageInYears = Math.floor(
-    ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25)
-  );
-  const ageInMonths = Math.floor(
-    (ageInMilliseconds % (1000 * 60 * 60 * 24 * 365.25)) /
-      (1000 * 60 * 60 * 24 * 30.4375)
-  );
-  const ageInDays = Math.floor(
-    (ageInMilliseconds % (1000 * 60 * 60 * 24 * 30.4375)) /
-      (1000 * 60 * 60 * 24)
-  );
+  // calculation of age
+  let years = new Date().getFullYear() - new Date(dob).getFullYear();
+  let months = new Date().getMonth() - new Date(dob).getMonth();
+  let days = new Date().getDate() - +D;
 
-  // Display result
-  yearsDiv.textContent = `${ageInYears} ${ageInYears === 1 ? "Year" : "Years"}`;
-  monthsDiv.textContent = `${ageInMonths} ${
-    ageInMonths === 1 ? "Month" : "Months"
-  }`;
-  daysDiv.textContent = `${ageInDays} ${ageInDays === 1 ? "Day" : "Days"}`;
-  resultDiv.classList.remove("hidden");
+  if (months < 0) {
+    years = years - 1;
+    months = months + 12;
+  }
+  if (days < 0) {
+    days += getNoOfDays(Y, M - 1);
+  }
+
+  dayOut.innerText = days;
+  monthOut.innerText = months;
+  yearOut.innerText = years;
 });
+
+const getNoOfDays = (y, m) => {
+  return new Date(y, m, 0).getDate();
+};
+
+// Validate Day
+function validDay(y, m, d) {
+  if (d > getNoOfDays(y, m) || d < 1) return false;
+  return true;
+}
+
+// validate Month
+function validMonth(m) {
+  if (m > 12 || m < 1) return false;
+  return true;
+}
+
+// Validate Year
+function validYear(y, m, d) {
+  const secondDate = new Date();
+  const firstDate = new Date(`${y}-${m}-${d}`);
+  if (firstDate.setHours(0, 0, 0, 0) <= secondDate.setHours(0, 0, 0, 0)) {
+    return true;
+  }
+  return false;
+}
+
+// days validation
+dayIn.addEventListener("blur", () => {
+  validateDay();
+});
+
+const validateDay = () => {
+  const D = dayIn.value;
+  const M = monthIn.value;
+  const Y = yearIn.value;
+  if (D == "") {
+    showMessage(dayIn, "This field is required", errorStyle);
+    return false;
+  } else if (!validDay(Y, M, D)) {
+    showMessage(dayIn, "Must be a valid day", errorStyle);
+    return false;
+  } else {
+    showMessage(dayIn, "", "");
+    return true;
+  }
+};
+
+// On Blur month validation
+monthIn.addEventListener("blur", () => {
+  validateMonth();
+});
+
+const validateMonth = () => {
+  const M = monthIn.value;
+  if (M == "") {
+    showMessage(monthIn, "This field is required", errorStyle);
+    return false;
+  } else if (!validMonth(M)) {
+    showMessage(monthIn, "Must be a valid month", errorStyle);
+    return false;
+  } else {
+    showMessage(monthIn, "", "");
+    return true;
+  }
+};
+
+// on Blur Year validate
+yearIn.addEventListener("blur", () => {
+  validateYear();
+});
+
+const validateYear = () => {
+  const Y = yearIn.value;
+  const M = monthIn.value;
+  const D = dayIn.value;
+  if (Y == "") {
+    showMessage(yearIn, "This field is required", errorStyle);
+    return false;
+  } else if (!validYear(Y, M, D)) {
+    showMessage(yearIn, "Must be in past", errorStyle);
+    return false;
+  } else {
+    showMessage(yearIn, "", "");
+    return true;
+  }
+};
+
+// Display Message
+function showMessage(elem, msg, border) {
+  elem.style.border = border;
+  elem.nextElementSibling.innerText = msg;
+}
