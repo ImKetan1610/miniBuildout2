426a28bb892ab30e772ee9b13566e33eb75f943d
ImKetan1610 <ketan.makode@gmail.com>
2024-05-13 11:09:12 +0530

    added html structure: need to make some alteration in the style

---
+++ expenses-chart-component-main/main.js
@@ -0,0 +1,9 @@
+let data;
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
