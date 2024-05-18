ba34e05fe061b070b9f5c77f266d6543d3f93ae9
ImKetan1610 <ketan.makode@gmail.com>
2024-05-18 13:15:51 +0530

    added logic for dropdowns

---
+++ intro-section-with-dropdown-navigation-main/index.js
@@ -0,0 +1,13 @@
+console.log("hey this message is coming from index.js");
+const menu = document.querySelector(".menu");
+const dropdown = document.querySelectorAll(".dropdown");
+
+menu.addEventListener("click", function () {
+  menu.parentElement.classList.toggle("open");
+  document.body.classList.toggle("nav-open");
+});
+dropdown.forEach(function (item) {
+  item.addEventListener("click", function () {
+    item.parentElement.classList.toggle("link-open");
+  });
+});
